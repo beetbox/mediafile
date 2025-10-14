5bfd2cb144ce618c7a8fff7a713addc661bd7465
Sebastian Mohr <sebastian@mohrenclan.de>
2025-10-14 12:01:10 +0200

    Moved afs unpack util functions to asf module.

--- mediafile/storage/afs.py
+++ mediafile/storage/afs.py
@@ -1,7 +1,43 @@
+import struct
+
 import mutagen
 import mutagen.asf
 
 from .base import ListStorageStyle
+
+
+def _unpack_asf_image(data):
+    """Unpack image data from a WM/Picture tag. Return a tuple
+    containing the MIME type, the raw image data, a type indicator, and
+    the image's description.
+
+    This function is treated as "untrusted" and could throw all manner
+    of exceptions (out-of-bounds, etc.). We should clean this up
+    sometime so that the failure modes are well-defined.
+    """
+    type, size = struct.unpack_from("<bi", data)
+    pos = 5
+    mime = b""
+    while data[pos : pos + 2] != b"\x00\x00":
+        mime += data[pos : pos + 2]
+        pos += 2
+    pos += 2
+    description = b""
+    while data[pos : pos + 2] != b"\x00\x00":
+        description += data[pos : pos + 2]
+        pos += 2
+    pos += 2
+    image_data = data[pos : pos + size]
+    return (mime.decode("utf-16-le"), image_data, type, description.decode("utf-16-le"))
+
+
+def _pack_asf_image(mime, data, type=3, description=""):
+    """Pack image data for a WM/Picture tag."""
+    tag_data = struct.pack("<bi", type, len(data))
+    tag_data += mime.encode("utf-16-le") + b"\x00\x00"
+    tag_data += description.encode("utf-16-le") + b"\x00\x00"
+    tag_data += data
+    return tag_data
 
 
 class ASFStorageStyle(ListStorageStyle):
